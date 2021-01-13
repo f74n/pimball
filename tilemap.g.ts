c0852badbf8b3f1fbf0223388099ca5591a29d2a
f74n <neofran@gmail.com>
2021-01-13 19:20:03 +0100

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,49 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers.registerTilemapFactory(function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level": return tiles.createTilemap(hex`100010000103030303030303030303030303030904020202020202020202020202020209040202020202020202020202020202090402020202020202020202020202020b040202020202020202020202020202070402020a020202020202020a020202070402020202020202020202020202090704020202020202020202020202020907040202020a02020202020a020202090704020202020202020202020202020907040202020202020202020202020209070402020202020202020202020202090704020202020202020202020202020907090909090202020202020202090909070402020202020202020202020202090705060606060606060606060606060908`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundSouthEast0,sprites.castle.saplingOak,sprites.castle.tilePath5,sprites.builtin.forestTiles0], TileScale.Sixteen)
+            case "level_0": return tiles.createTilemap(hex`100010000103030303030303030303030303030a0402020202020202020202020202020a0402020202020202020202020202020a0402020202020202020202020202020b040202020202020202020202020202070402020902020202020202090202020704020202020202020202020202020a0704020202020202020202020202020a0704020202090202020202090202020a070a020202020202020202020202020a070a0a02020202020202020202020a0a070a0a0a0202020202020202020a0a0a070a0a0a0a020202020202020a0a0a0a070a0a0a0a0a02020202020a0a0a0a0a0704020202020202020202020202020a0705060606060606060606060606060a08`, img`
+. . . . . . . . . . . . . . . 2 
+. . . . . . . . . . . . . . . 2 
+. . . . . . . . . . . . . . . 2 
+. . . . . . . . . . . . . . . 2 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . 2 . 
+. . . . . . . . . . . . . . 2 . 
+. . . . . . . . . . . . . . 2 . 
+2 . . . . . . . . . . . . . 2 . 
+2 2 . . . . . . . . . . . 2 2 . 
+2 2 2 . . . . . . . . . 2 2 2 . 
+2 2 2 2 . . . . . . . 2 2 2 2 . 
+2 2 2 2 2 . . . . . 2 2 2 2 2 . 
+. . . . . . . . . . . . . . 2 . 
+. . . . . . . . . . . . . . 2 . 
+`, [myTiles.transparency16,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundSouthEast0,sprites.castle.tilePath5,sprites.builtin.brick,sprites.castle.rock2], TileScale.Sixteen)
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
